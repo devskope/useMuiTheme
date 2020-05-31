384ca784433af06106f46d7aacfc66ba18ad1328
devskope <ope.oguntoye@protonmail.com>
2020-05-31 01:33:04 +0100

    [#4] create test components and hook configurations

---
+++ test/helpers/config.js
@@ -0,0 +1,17 @@
+export default {
+  customNameConfig: {
+    themeOptions: { name: 'sunset orange' },
+  },
+  deepMergeConfig: {
+    themeOptions: { name: 'sunset orange' },
+    themeArgs: [{ name: 'blue twilight' }, { palette: { type: 'dark' } }],
+  },
+  responsiveFontConfig: {
+    themeOptions: { name: 'sunset orange' },
+    responsiveFonts: true,
+  },
+
+  darkConfig: {
+    themeOptions: { name: 'dark mode', palette: { type: 'dark' } },
+  },
+};
